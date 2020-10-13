9e0d544e5f0f3decbb4f8c15466c368ee7d07319
vangaru <ivanenko0607@gmail.com>
2020-10-14 01:58:32 +0300

    added дополнение, замыкание, обратное отношение

--- DM labs/lab 2/src/funcs.py
+++ DM labs/lab 2/src/funcs.py
@@ -2,6 +2,7 @@ import itertools
 import numpy
 import math
 
+# ФУНКЦИЯ ВОЗВРАЩАЕТ ДЕКАРТОВО ПРОИЗВЕДЕНИЯ МНОЖЕСТВА
 def get_decart_mult(array):
 	decart_mult = []
 	for i in itertools.product(array, array):
@@ -9,52 +10,88 @@ def get_decart_mult(array):
 	
 	return decart_mult
 
+# ФУНКЦИЯ ВОЗВРАЩАЕТ TRUE, ЕСЛИ ПАРА ДЕКАРТОВА ПРОИЗВЕДЕНИЯ
+# ОТНОСИТСЯ К ОТНОШЕНИЮ R1
 def R1_build(pair):
 	if pair[0] % 4 == pair[1] % 4:
 		return True
 	else:
 		return False
 
+# ФУНКЦИЯ ВОЗВРАЩАЕТ TRUE, ЕСЛИ ПАРА ДЕКАРТОВА ПРОИЗВЕДЕНИЯ
+# ОТНОСИТСЯ К ОТНОШЕНИЮ R1
 def R2_build(pair):
 	if pair[0] + pair[1] == 8:
 		return True
 	else:
 		return False
 
-def R1_matrix_build(array):
+# ФУНКЦИЯ ВОЗВРАЩАЕТ ОТНОШЕНИЕ R1
+def get_R1(array):
 	decart_mult = get_decart_mult(array)
-	
-	matrix = numpy.zeros((len(array) + 1, len(array) + 1), dtype=numpy.int)
-
-	matrix[0] = range(len(array) + 1)
-
-	for i in range(len(array) + 1):
-		matrix[i][0] = i
+	R1 = []
 
 	for pair in decart_mult:
 		if R1_build(pair) == True:
-			matrix[pair[0]][pair[1]] = 1
-		else:
-			matrix[pair[0]][pair[1]] = 0
+			R1.append(pair)
 
-	print(matrix)
+	return R1
 
-
-
-def R2_matrix_build(array):
+# ФУНКЦИЯ ВОЗВРАЩАЕТ ОТНОШЕНИЕ R2
+def get_R2(array):
 	decart_mult = get_decart_mult(array)
-	
-	matrix = numpy.zeros((len(array) + 1, len(array) + 1), dtype=numpy.int)
-
-	matrix[0] = range(len(array) + 1)
-
-	for i in range(len(array) + 1):
-		matrix[i][0] = i
+	R2 = []
 
 	for pair in decart_mult:
 		if R2_build(pair) == True:
-			matrix[pair[0]][pair[1]] = 1
-		else:
-			matrix[pair[0]][pair[1]] = 0
+			R2.append(pair)
 
-	print(matrix)
+	return R2
+
+# ФУНКЦИЯ ВОЗВРАЩАЕТ МАТРИЦУ ОТНОШЕНИЙ
+def matrix_build(R, array):
+
+	matrix = numpy.zeros((len(array), len(array)), dtype=numpy.int)
+
+	for pair in R:
+		matrix[pair[0] - 1][pair[1] - 1] = 1 
+
+	return matrix
+
+# ФУНКЦИЯ ВОЗВРАЩАЕТ ОБРАТНОЕ ОТНОШЕНИЕ 
+def get_inverse_relation(R):
+	inverse_relation = []
+
+	for pair in R:
+		temp_pair = pair
+		pair = []
+
+		pair.append(temp_pair[1])
+		pair.append(temp_pair[0])
+
+		inverse_relation.append(pair)
+
+	return inverse_relation
+
+# ФУНКЦИЯ ВОЗВРАЩАЕТ ДОПОЛНЕНИЕ ОТНОШЕНИЯ
+def get_relation_addition(array, R):
+	decart_mult = get_decart_mult(array)
+	relation_addition = []
+
+	for pair in decart_mult:
+		if pair not in R:
+			relation_addition.append(pair)
+
+	return relation_addition 
+
+def get_transitive_closure(matrix):
+	n = len(matrix)
+	transitive_closure = matrix 
+	
+	for k in range(n):
+		for i in range(n):
+			for j in range(n):
+				if (matrix[i][k] and matrix[k][j] == True):
+					transitive_closure[i][j] = 1
+				
+	return transitive_closure
--- DM labs/lab 2/src/main.py
+++ DM labs/lab 2/src/main.py
@@ -4,8 +4,25 @@ import funcs
 
 A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-#funcs.R1_matrix_build(A)
+#ЗАДАНИЕ 1
+print("МАТРИЦА ОТНОШЕНИЙ R1")
+print(funcs.matrix_build(R = funcs.get_R1(A), array = A))
 
-funcs.R1_matrix_build(A)
-print("\n\n\n")
-funcs.R2_matrix_build(A)
+print("\n\n\nМАТРИЦА ОТНОШЕНИЙ R2")
+print(funcs.matrix_build(R = funcs.get_R2(A), array = A))
+
+#ЗАДАНИЕ 2
+print("ОБРАТНОЕ ОТНОШЕНИЕ R1")
+print(funcs.get_inverse_relation(R = funcs.get_R1(A)))
+
+print("ОБРАТНОЕ ОТНОШЕНИЕ R2")
+print(funcs.get_inverse_relation(R = funcs.get_R2(A)))
+
+print("ДОПОЛНЕНИЕ ОТНОШЕНИЯ R1")
+print(funcs.get_relation_addition(array = A, R = funcs.get_R1(A)))
+
+print("ДОПОЛНЕНИЕ ОТНОШЕНИЯ R2")
+print(funcs.get_relation_addition(array = A, R = funcs.get_R2(A)))
+
+print("ТРАНЗИТИВНОЕ ЗАМЫКАНИЕ ОТНОШЕНИЯ R2")
+print(funcs.get_transitive_closure(matrix = funcs.matrix_build(R = funcs.get_R2(A), array = A)))
